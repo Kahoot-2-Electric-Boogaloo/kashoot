d8f85865e9831a365fd87c608a40ff545bd6e373
Cosmin Mihart <mihartcosmin80@gmail.com>
2024-01-02 19:40:31 +0200

    Moved the join game form into a component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,9 @@
-import Image from "next/image";
+import JoinGameForm from "@/components/joinGameForm";
 
 export default function Home() {
   return (
     <main>
-      <div className='flex justify-center h-screen items-center'>
-        <form className='grid grid-rows-2 gap-4 p-2 justify-center w-64 h-32 bg-background rounded-md'>
-          <input
-            type='text'
-            placeholder='Game PIN'
-            className='block text-center
-            border-2
-            border-white
-            border-solid
-            bg-background-alt
-            font-bold
-            rounded-md
-            hover:border-accent
-            focus:border-accent
-            transition duration-[100] ease-in-out
-            '
-          />
-          <button
-            className='bg-background-alt
-            rounded-md
-            outline-none
-            shadow-[0_3px_0px_0px_rgba(255,255,255)]
-            font-bold
-            hover:bg-accent
-            hover:shadow-[0_2px_0px_0px_rgba(255,255,255)]
-            hover:translate-y-[2px]
-            active:shadow-none
-            active:translate-y-[4px]
-            transition duration-[100] ease-in-out
-            '
-          >
-            Enter
-          </button>
-        </form>
-      </div>
+      <JoinGameForm />
     </main>
   );
 }
