cb24c78aee0aa1cdc224da42efa1bcf9bdb74b2a
Cosmin Mihart <mihartcosmin80@gmail.com>
2024-01-06 12:37:59 +0200

    Created display components for all question types

---
+++ src/components/quiz-elements/questions-display/textInputDisplay.tsx
@@ -0,0 +1,27 @@
+export interface TextInputDisplayProps {
+  content: string;
+}
+
+const buttonClassName =
+  " rounded-md font-semibold text-xl border-solid hover:border-2 hover:brightness-[1.2] active:scale-[.97] duration-75 ease-linear w-8/12 ml-auto mr-auto";
+
+export default function TextInputdDisplay(props: TextInputDisplayProps) {
+  return (
+    <div className='flex flex-col h-[100%] w-[100%] justify-center items-center relative'>
+      <h1 className='text-3xl font-semibold absolute left-4 top-8 w-11/12 indent-8 select-none'>
+        {props.content}
+      </h1>
+      <div className='grid grid-cols-1 grid-rows-2 w-10/12 absolute bottom-20 gap-4'>
+        <input
+          className={
+            "bg-background-alt rounded-xl text-center text-2xl font-semibold hover:border-4 border-accent"
+          }
+          style={{ minHeight: "5rem" }}
+          type='text'
+          placeholder='Type your answer...'
+        />
+        <button className={"bg-secondary" + buttonClassName}>Submit</button>
+      </div>
+    </div>
+  );
+}
