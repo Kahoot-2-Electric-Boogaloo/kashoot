53de478b77af04c58aecb9212d443f5064a637cc
marc <fmarcandrei@gmail.com>
2024-01-10 23:12:33 +0200

    last

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { getDocs, collection } from 'firebase/firestore';
-import { firestore } from '@/firebase/config'; // Import firestore from your firebase module
+import { firestore } from '@/firebase/config';
 
 const QuizPage: React.FC = () => {
     const [quizzes, setQuizzes] = useState<{ id: string; quizName: string; userName: string; questionsCount: number }[]>([]);
+    const [copiedQuizId, setCopiedQuizId] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchQuizzes = async () => {
@@ -29,16 +30,37 @@ const QuizPage: React.FC = () => {
         fetchQuizzes();
     }, []);
 
+    const handleCopyQuizId = (quizId: string) => {
+        navigator.clipboard.writeText(quizId);
+        setCopiedQuizId(quizId);
+
+        // Clear the message after a few seconds (e.g., 3 seconds)
+        setTimeout(() => {
+            setCopiedQuizId(null);
+        }, 1000);
+    };
+
     return (
         <div className="container mx-auto mt-8">
             <h1 className="text-2xl font-bold mb-4">Quizzes</h1>
             {!quizzes.length ? <p>No quizzes available.</p> : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                     {quizzes.map((quiz) => (
-                        <div key={quiz.id} className="bg-background hover:border hover:bg-accent ease-linear duration-150 cursor-pointer p-4 shadow-md rounded-md">
-                            <h2 className="text-lg font-semibold">{quiz.quizName}</h2>
-                            <p className="text-gray-600 mb-2">Creator: {quiz.userName}</p>
-                            <p className="text-gray-600">Questions Count: {quiz.questionsCount}</p>
+                        <div
+                            key={quiz.id}
+                            className="bg-background text-accent hover:text-white hover:bg-accent ease-linear duration-150 cursor-pointer p-4 shadow-md rounded-md relative"
+                            onClick={() => handleCopyQuizId(quiz.id)}
+                        >
+                            <h1 className="text-2xl font-semibold mb-2">{quiz.quizName}</h1>
+                            <p className="mb-2">Creator: {quiz.userName}</p>
+                            <p className="mb-2">Questions Count: {quiz.questionsCount}</p>
+                            <p className="">Game Pin: {quiz.id}</p>
+
+                            {copiedQuizId === quiz.id && (
+                                <div className="ease-linear font-bold rounded-md text-2xl flex items-center justify-center absolute top-0 left-0 right-0 bottom-0 bg-green-500 text-white opacity-80 p-2">
+                                    Quiz ID Copied!
+                                </div>
+                            )}
                         </div>
                     ))}
                 </div>
